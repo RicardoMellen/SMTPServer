ac525ae006dd46d4500617b5cce5edcb39a596b9
RicardoMellen <ricardo.mellen@gmail.com>
2015-06-05 00:10:02 +0200

    Update Messages.java

--- src/Messages.java
+++ src/Messages.java
@@ -3,19 +3,22 @@
 public class Messages {
 
 	// Connection
-	String serverReady = "220  RicardoServer Service Ready \r\n";
-	String serverConnection = "250 Requested mail action okay, completed \r\n";
+	static String serverReady = "220  RicardoServer Service Ready \r\n";
+	static String serverConnection = "250 Requested mail action okay, completed \r\n";
 
 	// Sending an e-mail
-	String serverOK = "250 OK \r\n";
-	String serverData = "354 Start mail input; end with <CRLF>.<CRLF> \r\n";
+	static String serverOK = "250 OK \r\n";
+	static String serverData = "354 Start mail input; end with <CRLF>.<CRLF> \r\n";
 
 	// Closing the connection
-	String serverClose = "221 RicardoServer Service closing transmission channel \r\n";
+	static String serverClose = "221 RicardoServer Service closing transmission channel \r\n";
 
 	// Error reply codes
-	String serverCommand = "502 Syntax error, command unrecognized \r\n";
-	String serverParameters = "501 Syntax error in parameters or arguments \r\n";
-	String serverSequence = "503 Bad sequence of commands \r\n";
-	String serverRecipients = "554 Not valid recipients (Only after DATA command) \r\n";
+	static String serverCommand = "502 Syntax error, command unrecognized \r\n";
+	static String serverParameters = "501 Syntax error in parameters or arguments \r\n";
+	static String serverSequence = "503 Bad sequence of commands \r\n";
+	static String serverRecipients = "554 Not valid recipients (Only after DATA command) \r\n";
+	
+	static String logs = "LOGS/";
+	static String txt = ".txt";
 }
